b63d9e28efc0aafc710c12716bbb99a22101fa2d
SushmaSingh1 <56636424+SushmaSingh1@users.noreply.github.com>
2019-11-16 12:03:04 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,2 @@
+email_address=raw_input()
+print(email_address[email_address.find('@')+1:email_address.find('.')])
---
+++ 8.py
@@ -0,0 +1,2 @@
+square_numbers=map(lambda x: x*x ,range(1,21))
+print(list(square_numbers))
